2bd898bf4ff90e0d8802d3ded39c827d1798e9ed
senzor <senzor@cse20105.cs.local>
2015-05-15 00:01:43 -0400

    finished-ish

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import socket, sys, time
+import socket, sys, time, calendar
 from thread import *
 
 # config = open(config.txt)
@@ -27,31 +27,66 @@ print 'Socket bind complete'
 #Start listening on socket
 s.listen(10)
 print 'Socket now listening'
-
+gameVariable = False
+gameStartTime = -1 #time that the next game
+shotFrequency = -1
 stats = {}
 team1 = {}
 team2 = {}
 
+def toEpoch(timestamp):
+	split = str(timestamp).split(' ')
+	tdate = map(int,split[1].split('-'))
+	ttime = map(int,split[2].split(':'))
+	tcode = (tdate[0], tdate[1], tdate[2], ttime[0], ttime[1], ttime[2])
+	epoch = calendar.timegm(tcode)
+	return (int(epoch))
+
+def timeUntilNextGame():
+	if gameStartTime != -1:
+	    return str(gameStartTime - time.time())
+
+def scheduleGame(gameStart, gameLength, gameStartTime, maxShots, shotFrequency):
+	time.sleep(gameStart)
+	startGame(gameLength, maxShots, shotFrequency)
+
+def startGame(gameLength, maxShots, shotFrequency):
+	global gameVariable
+	gameVariable = True
+	print "game started!"
+	time.sleep(gameLength) #NOTE: Multiply by 60 later (convert to min)
+	endGame()
+
 def gameLogic():
 	while 1:
-		print "1. quit\n2. start game"
-		command = input("Enter command: ")
+		print "1. quit\n2. start game\n3. schedule game"
+		command = input("Enter command: \n")
         	if int(command) == 1:
                 	print "quit!"
 			break
         	elif int(command) == 2:
                 	gameLength = int(input("How long should this game last? (min) "))
-                	print time.time()
-			start_new_thread(startGame, (gameLength,))
-			#maxshots = input("How many shots are allowed per player per game? ")
-maxshots = 5
-def startGame(gameLength):
-	print "gamestarted!"
-	time.sleep(gameLength)
-	endGame()
+			global maxShots
+			maxShots = int(input("How many shots are allowed per player per game? "))
+			global shotFrequency
+			shotFrequency = int(input("How many shots are allowed per second? "))
+			start_new_thread(startGame, (gameLength, maxShots, shotFrequency))
+		elif int(command) == 3:
+			gameStart = int(input("How long until next game starts? (min) "))
+			gameStart *= 60
+			global gameStartTime 
+			gameStartTime= gameStart + time.time()
+			gameLength = int(input("How long should this game last? (min) "))
+			maxShots = int(input("How many shots are allowed per player per game? "))
+			shotFrequency = int(input("How many shots are allowed per second? "))
+			start_new_thread(scheduleGame, (gameStart, gameLength, gameStartTime, maxShots, shotFrequency))
+		else:
+		    print "You fail... Try 1-3."
 
 def endGame():
-	print "endgame!"
+	print "end game!"
+	global gameVariable
+	gameVariable = False
 
 #Function for handling connections. This will be used to create threads
 def clientthread(conn, username):
@@ -63,30 +98,68 @@ def clientthread(conn, username):
 
 		#Receiving from client
 		data = conn.recv(1024)
+		global shotFrequency
 		if not data:
 			break
-                data = data.rstrip()
+		global gameVariable
+                if not gameVariable:
+			#if timeUntilNextGame() != -1:
+			#	conn.sendall("No game in progress. Next game starts in "+ timeUntilNextGame() + " seconds.")
+			#else:
+			#	conn.sendall("No game in progress.")
+			break
+		data = data.rstrip()
 		print username+': '+data
 		clientVariables = data.split(",") #client variables = splitted data
 		if clientVariables[0] == "shot":
+		   
 		    if username in team1:
-			if len(team1[username]["shots"]) <= maxshots:
-		    		team1[username]["shots"].append(clientVariables[1])    
+			if len(team1[username]["shots"]) < maxShots: 
+			    if len(team1[username]["shots"]) == 0: 
+				team1[username]["shots"].append(toEpoch(clientVariables[1]))
+			    else:
+				if int(toEpoch(clientVariables[1])) - int(team1[username]["shots"][-1]) >= shotFrequency:
+		    			team1[username]["shots"].append(toEpoch(clientVariables[1]))
+			else:
+			    break
 		    else:
-			if len(team2[username]["shot"]) <= maxshots:
-				team2[username]["shots"].append(clientVariables[1])
+			if len(team2[username]["shot"]) < maxShots:
+			    if len(team2[username]["shots"]) == 0:
+				team2[username]["shots"].append(toEpoch(clientVariables[1]))
+			    else:
+				if int(toEpoch(clientVariables[1])) - int(team2[username]["shots"][-1]) >= shotFrequency:
+					team2[username]["shots"].append(toEpoch(clientVariables[1]))
+			else:
+			    break
 		if clientVariables[0] == "hit":
 		    if int(username)%2 == 1:	#if odd then
 			if int(clientVariables[2])%2 != int(username)%2:
-		    		team1[username]["hits"].append([clientVariables[1],clientVariables[2]])
+		    		team1[username]["hits"].append([toEpoch(clientVariables[1]),clientVariables[2]])
 		    else:
-			if int(clientVariable[2])%2 != int(username)%2:
-		    		team2[username]["hits"].append([clientVariables[1],clientVariables[2]])
+			if int(clientVariables[2])%2 != int(username)%2:
+		    		team2[username]["hits"].append([toEpoch(clientVariables[1]),clientVariables[2]])
+		# print clientVariables[1] - team1[username]["shots"][-1]  
+		# print shotFrequency
+ 		# print clientVariables[1] - team2[username]["shots"][-1]  
+		# print shotFrequency
+
 		print team1
 		print team2
-		if clientVariables[0] == "no game":
-		    print "no game"
-		conn.sendall('got it!')
+		print "Team 1 Stats: "
+		for playerID in team1:
+			print "Gun ",playerID,":" 
+			print "     Shot count = ", len(team1[playerID]["shots"]) 
+			print "     Hit count = ", len(team1[playerID]["hits"])
+		print "Team 2 Stats: "
+		for playerID2 in team2:
+			print "Gun ",playerID2,":" 
+			print "     Shot count = ", len(team2[playerID2]["shots"])
+			print "     Hit count = ", len(team2[playerID2]["hits"])
+		print
+		
+		if clientVariables[0] == "error":
+		    	print "error"
+			conn.sendall('got it!')
 
 
 	#came out of loop
@@ -94,12 +167,12 @@ def clientthread(conn, username):
 
 #now keep talking with the client
 
-start_new_thread(gameLogic, ())
+start_new_thread(gameLogic, ()) #comma cause tuple
 
 while 1:
     #wait to accept a connection - blocking call
 	conn, addr = s.accept()
-        #Get username from client
+	#Get username from client
         username = conn.recv(1024)
         if not username:
                 print('Username not recieved')
